73cf1982a9a7ca5eb2232e49bcfa06ea1ef26fcd
Clark Perkins <clark.perkins@digitalreasoning.com>
2015-04-14 11:37:10 -0500

    BLUE-46 added HADOOP_CLIENT_OPTS

--- cdh5/etc/hadoop/conf/hadoop-env.sh
+++ cdh5/etc/hadoop/conf/hadoop-env.sh
@@ -1,4 +1,5 @@
 
 {% if salt['pillar.get']('cdh5:security:enable', False) %}
 export HADOOP_OPTS="$HADOOP_OPTS -Djava.security.krb5.conf={{ pillar.krb5.conf_file }}"
+export HADOOP_CLIENT_OPTS="$HADOOP_CLIENT_OPTS -Djava.security.krb5.conf={{ pillar.krb5.conf_file }}"
 {% endif %}
