4ee1414309f00220f9abdfa051e6666124bad038
Clark Perkins <clark.perkins@digitalreasoning.com>
2016-05-06 10:53:00 -0500

    Fixed JAVA_HOME

--- cdh5/etc/hadoop/conf/hadoop-env.sh
+++ cdh5/etc/hadoop/conf/hadoop-env.sh
@@ -1,4 +1,6 @@
 
+export JAVA_HOME="/usr/java/latest"
+
 {% if salt['pillar.get']('cdh5:security:enable', False) %}
 export HADOOP_OPTS="$HADOOP_OPTS -Djava.security.krb5.conf={{ pillar.krb5.conf_file }}"
 export HADOOP_CLIENT_OPTS="$HADOOP_CLIENT_OPTS -Djava.security.krb5.conf={{ pillar.krb5.conf_file }}"
