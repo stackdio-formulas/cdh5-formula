50b22a4b75bd3702b4aa25c522c72bf04151831c
Clark Perkins <clark.perkins@digitalreasoning.com>
2016-09-12 11:39:52 -0500

    Added if conditions in the bash generate scripts

--- cdh5/security/generate_http_keytab.sh
+++ cdh5/security/generate_http_keytab.sh
@@ -1,6 +1,11 @@
 {%- from 'krb5/settings.sls' import krb5 with context %}
 {%- set realm = krb5.realm -%}
 #!/bin/bash
+
+if [ -f HTTP.keytab ]; then
+   exit 0
+fi
+
 export KRB5_CONFIG={{ pillar.krb5.conf_file }}
 
 (
