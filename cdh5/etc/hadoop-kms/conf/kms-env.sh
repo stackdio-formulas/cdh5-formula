b997bd61d82aac72f2f571f73242dc045a453974
Clark Perkins <clark.perkins@digitalreasoning.com>
2018-05-30 16:06:23 -0500

    Actually uncomment things

--- cdh5/etc/hadoop-kms/conf/kms-env.sh
+++ cdh5/etc/hadoop-kms/conf/kms-env.sh
@@ -75,19 +75,19 @@ export CATALINA_OPTS="$CATALINA_OPTS -Djava.security.krb5.conf={{ pillar.krb5.co
 {% if pillar.cdh5.encryption.enable %}
 # The location of the SSL keystore if using SSL
 #
-# export KMS_SSL_KEYSTORE_FILE=/etc/hadoop-kms/conf/kms.keystore
+export KMS_SSL_KEYSTORE_FILE=/etc/hadoop-kms/conf/kms.keystore
 
 # The password of the SSL keystore if using SSL
 #
-# export KMS_SSL_KEYSTORE_PASS=hadoopkms
+export KMS_SSL_KEYSTORE_PASS=hadoopkms
 
 # The location of the SSL keystore if using SSL
 #
-# export KMS_SSL_TRUSTSTORE_FILE=/etc/hadoop-kms/conf/kms.truststore
+export KMS_SSL_TRUSTSTORE_FILE=/etc/hadoop-kms/conf/kms.truststore
 
 # The password of the SSL keystore if using SSL
 #
-# export KMS_SSL_TRUSTSTORE_PASS=hadoopkms
+export KMS_SSL_TRUSTSTORE_PASS=hadoopkms
 {% endif %}
 
 # The full path to any native libraries that need to be loaded
